730225a252cca41b82617ba5ae1aabc7a2c80b39
FikalAhmad <fikaalm26@gmail.com>
2024-07-21 04:38:48 +0700

    feat: add middleware to check roles

--- routes/PelangganRoute.js
+++ routes/PelangganRoute.js
@@ -7,15 +7,45 @@ import {
   LoginPelanggan,
   updatePelanggan,
 } from "../controller/PelangganController.js";
+import { authenticateUser, authorizeRole } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/pelanggan", getPelanggan);
-router.get("/pelanggan/:id", getPelangganById);
-router.post("/pelanggan", createPelanggan);
-router.patch("/pelanggan/:id", updatePelanggan);
-router.delete("/pelanggan/:id", deletePelanggan);
-router.post("/login", LoginPelanggan);
-// router.delete("/logout", Logout);
+router.get(
+  "/pelanggan",
+  authenticateUser,
+  authorizeRole(["Admin", "Petugas"]),
+  getPelanggan
+);
+router.get(
+  "/pelanggan/:id",
+  authenticateUser,
+  authorizeRole(["Admin", "Petugas"]),
+  getPelangganById
+);
+router.post(
+  "/pelanggan",
+  authenticateUser,
+  authorizeRole(["Admin", "Petugas"]),
+  createPelanggan
+);
+router.patch(
+  "/pelanggan/:id",
+  authenticateUser,
+  authorizeRole(["Admin", "Petugas"]),
+  updatePelanggan
+);
+router.delete(
+  "/pelanggan/:id",
+  authenticateUser,
+  authorizeRole(["Admin", "Petugas"]),
+  deletePelanggan
+);
+router.post(
+  "/login",
+  authenticateUser,
+  authorizeRole(["Admin", "Petugas"]),
+  LoginPelanggan
+);
 
 export default router;
