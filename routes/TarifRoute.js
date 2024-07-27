5ce40f0735c7c46cd3e1326ef8383dcb338157a7
FikalAhmad <fikaalm26@gmail.com>
2024-07-27 17:19:43 +0700

    chore: add authorize and authenticate

--- routes/TarifRoute.js
+++ routes/TarifRoute.js
@@ -6,13 +6,39 @@ import {
   getTarifById,
   updateTarif,
 } from "../controller/TarifController.js";
+import { authenticateUser, authorizeRole } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/tarif", getTarif);
-router.get("/tarif/:id", getTarifById);
-router.post("/tarif", createTarif);
-router.patch("/tarif/:id", updateTarif);
-router.delete("/tarif", deleteTarif);
+router.get(
+  "/tarif",
+  getTarif,
+  authenticateUser,
+  authorizeRole(["Admin", "Petugas", "Pelanggan"])
+);
+router.get(
+  "/tarif/:id",
+  getTarifById,
+  authenticateUser,
+  authorizeRole(["Admin", "Petugas", "Pelanggan"])
+);
+router.post(
+  "/tarif",
+  createTarif,
+  authenticateUser,
+  authorizeRole(["Admin", "Petugas", "Pelanggan"])
+);
+router.patch(
+  "/tarif/:id",
+  updateTarif,
+  authenticateUser,
+  authorizeRole(["Admin", "Petugas", "Pelanggan"])
+);
+router.delete(
+  "/tarif",
+  deleteTarif,
+  authenticateUser,
+  authorizeRole(["Admin", "Petugas", "Pelanggan"])
+);
 
 export default router;
