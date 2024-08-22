a088f3fc34331b034b4b5eaa61c79eb97c376c25
FikalAhmad <fikaalm26@gmail.com>
2024-08-22 20:51:19 +0700

    fix: add pg dialect module

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,9 @@
 import { Sequelize } from "sequelize";
+import pg from "pg";
 
 const db = new Sequelize(process.env.CONNECTION_URI, {
   dialect: process.env.DIALECT,
+  dialectModule: pg,
 });
 
 export default db;
