b0e1358b62c078a7a8cfc4c6a65905516af64493
Ahmad Fikri Haikal <87348156+FikalAhmad@users.noreply.github.com>
2024-08-26 22:14:19 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,12 +1,7 @@
 import { Sequelize } from "sequelize";
-import * as pg from "pg";
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const db = new Sequelize(process.env.CONNECTION_URI, {
   dialect: process.env.DIALECT,
-  dialectModule: pg,
 });
 
 export default db;
